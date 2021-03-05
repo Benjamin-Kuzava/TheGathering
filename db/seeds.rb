8f72d4bc4a4e00d1328e8a67c949b2d525f059a5
Benjamin-Kuzava <kuzav1bj@gmail.com>
2021-03-05 13:49:59 -0500

    Basic seed data added

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,122 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-
 Category.destroy_all
 Article.destroy_all
 User.destroy_all
 
-
 @ben = User.create!(username: 'ben', email: 'ben@ben.io', password: '123456')
 @bob = User.create!(username: 'bob', email: 'bob@bob.io', password: '123456')
-@billy = User.create!(username: 'billy', email: 'billy@billy.io', password: '123456')
-@barbara = User.create!(username: 'barbara', email: 'barbara@barbara.io', password: '123456')
-
+@billy =
+  User.create!(username: 'billy', email: 'billy@billy.io', password: '123456')
+@barbara =
+  User.create!(
+    username: 'barbara',
+    email: 'barbara@barbara.io',
+    password: '123456',
+  )
 
 puts "#{User.count} users created."
+
+@standard = Category.create!(name: 'standard')
+@modern = Category.create!(name: 'modern')
+@pioneer = Category.create!(name: 'pioneer')
+@vintage = Category.create!(name: 'vintage')
+@pauper = Category.create!(name: 'pauper')
+@commander = Category.create!(name: 'commander')
+@budget = Category.create!(name: 'budget')
+@miscellaneous = Category.create!(name: 'miscellaneous')
+
+@format = [@standard, @modern, @pioneer, @vintage, @pauper, @commander]
+
+puts "#{Category.count} categories created."
+
+Article.create!(
+  title: 'This is a title',
+  content: Faker::Lorem.paragraphs(number: 4),
+  img_url: 'https://source.unsplash.com/random',
+  user: User.all.sample,
+  categories: [@format.sample, @budget],
+)
+
+Article.create!(
+  title: 'This is a title',
+  content: Faker::Lorem.paragraphs(number: 4),
+  img_url: 'https://source.unsplash.com/random',
+  user: User.all.sample,
+  categories: [@format.sample, @budget],
+)
+
+Article.create!(
+  title: 'This is a title',
+  content: Faker::Lorem.paragraphs(number: 4),
+  img_url: 'https://source.unsplash.com/random',
+  user: User.all.sample,
+  categories: [@format.sample, @budget],
+)
+
+Article.create!(
+  title: 'This is a title',
+  content: Faker::Lorem.paragraphs(number: 4),
+  img_url: 'https://source.unsplash.com/random',
+  user: User.all.sample,
+  categories: [@format.sample, @budget],
+)
+
+Article.create!(
+  title: 'This is a title',
+  content: Faker::Lorem.paragraphs(number: 4),
+  img_url: 'https://source.unsplash.com/random',
+  user: User.all.sample,
+  categories: [@format.sample, @budget],
+)
+
+Article.create!(
+  title: 'This is a title',
+  content: Faker::Lorem.paragraphs(number: 4),
+  img_url: 'https://source.unsplash.com/random',
+  user: User.all.sample,
+  categories: [@format.sample, @budget],
+)
+
+Article.create!(
+  title: 'This is a title',
+  content: Faker::Lorem.paragraphs(number: 4),
+  img_url: 'https://source.unsplash.com/random',
+  user: User.all.sample,
+  categories: [@format.sample, @miscellaneous],
+)
+
+Article.create!(
+  title: 'This is a title',
+  content: Faker::Lorem.paragraphs(number: 4),
+  img_url: 'https://source.unsplash.com/random',
+  user: User.all.sample,
+  categories: [@format.sample],
+)
+
+Article.create!(
+  title: 'This is a title',
+  content: Faker::Lorem.paragraphs(number: 4),
+  img_url: 'https://source.unsplash.com/random',
+  user: User.all.sample,
+  categories: [@format.sample],
+)
+
+Article.create!(
+  title: 'This is a title',
+  content: Faker::Lorem.paragraphs(number: 4),
+  img_url: 'https://source.unsplash.com/random',
+  user: User.all.sample,
+  categories: [@format.sample],
+)
+
+Article.create!(
+  title: 'This is a title',
+  content: Faker::Lorem.paragraphs(number: 4),
+  img_url: 'https://source.unsplash.com/random',
+  user: User.all.sample,
+  categories: [@format.sample],
+)
+
+puts "#{Article.count} articles created."

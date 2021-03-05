dad4d594fb931ebf4f0e3fab008155e68221422e
Benjamin-Kuzava <kuzav1bj@gmail.com>
2021-03-05 11:49:50 -0500

    -Finished auth in controllers -Added auth routes -updated associations in models

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :update, :destroy]
+  before_action :authorize_request, except: [:index, :show]
 
   # GET /articles
   def index

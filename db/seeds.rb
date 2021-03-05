f0028d07290c998b24284fce3fedc4e88ddf9c2b
Benjamin-Kuzava <kuzav1bj@gmail.com>
2021-03-05 12:28:40 -0500

    Users in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Category.destroy_all
+Article.destroy_all
+User.destroy_all
+
+
+@ben = User.create!(username: 'ben', email: 'ben@ben.io', password: '123456')
+@bob = User.create!(username: 'bob', email: 'bob@bob.io', password: '123456')
+@billy = User.create!(username: 'billy', email: 'billy@billy.io', password: '123456')
+@barbara = User.create!(username: 'barbara', email: 'barbara@barbara.io', password: '123456')
+
+
+puts "#{User.count} users created."

7509447bc74cae834e7059dbf8914e5c36dd289c
Benjamin-Kuzava <kuzav1bj@gmail.com>
2021-03-29 19:44:19 -0400

    Added loading functionality for article cards git push origin dev

--- client/src/components/ArticleCard/ArticleCard.jsx
+++ client/src/components/ArticleCard/ArticleCard.jsx
@@ -1,6 +1,7 @@
 import { Grid } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import Card from "@material-ui/core/Card";
+import Skeleton from "@material-ui/lab/Skeleton";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
@@ -32,34 +33,54 @@ const ArticleCard = (props) => {
         component={Link}
         to={`/articles/${props.article.id}`}
       >
-        <CardMedia
-          className={media}
-          image={props.article.img_url}
-          title={`Image for: ${props.article.title}`}
-        />
+        {props.article ? (
+          <CardMedia
+            className={media}
+            image={props.article.img_url}
+            title={`Image for: ${props.article.title}`}
+          />
+        ) : (
+          <Skeleton
+            variant="rect"
+            animation="wave"
+            width={"100%"}
+            height={118}
+          />
+        )}
         <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
-            {props.article.title}
-          </Typography>
+          {props.article ? (
+            <Typography gutterBottom variant="h5" component="div">
+              {props.article.title}
+            </Typography>
+          ) : (
+            <>
+              <Skeleton animation="wave" height={10} />
+              <Skeleton animation="wave" height={10} />
+              <Skeleton animation="wave" width="80%" height={10} />
+              <Skeleton animation="wave" width="80%" height={10} />
+            </>
+          )}
           <Typography variant="body2" color="textSecondary" component="div">
             {props.article.summary}
           </Typography>
         </CardContent>
       </CardActionArea>
       <CardActions>
-        <Grid container justify="space-between" alignItems="center">
-          <Button
-            size="small"
-            color="primary"
-            component={Link}
-            to={`/articles/${props.article.id}`}
-          >
-            Read More
-          </Button>
-          <Typography variant="caption" component="div">
-            {formatDate(props.article.created_at)}
-          </Typography>
-        </Grid>
+        {props.article.title && (
+          <Grid container justify="space-between" alignItems="center">
+            <Button
+              size="small"
+              color="primary"
+              component={Link}
+              to={`/articles/${props.article.id}`}
+            >
+              Read More
+            </Button>
+            <Typography variant="caption" component="div">
+              {formatDate(props.article.created_at)}
+            </Typography>
+          </Grid>
+        )}
       </CardActions>
     </Card>
   );
